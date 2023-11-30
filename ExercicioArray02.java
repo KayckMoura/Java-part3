567ca86342d1a470dcf98c162ae6df9ccc3ac3c2
Kayck Gabriel <145925201+KayckMoura@users.noreply.github.com>
2023-11-30 10:42:34 -0300

    Add files via upload

---
+++ ExercicioArray02.java
@@ -0,0 +1,19 @@
+
+public class ExercicioArray02 {
+
+	public static void main(String[] args) {
+		int[] arrayUm = {12,3,5,68,9,6,73,44,456,65,321};
+		int[] arrayDois = {43,42,4,8,55,21,2,45};
+		
+		if(arrayDois.length > 8) {
+			System.out.println("Tamanho arrayDois - maior que 8!");
+			
+		}else {
+			System.out.println("Tamanho ArrayDois - menor que 8!");
+
+
+		System.out.println("\nTamanho do ArrayUm = " + arrayUm.length);
+}			
+}	
+}
+
